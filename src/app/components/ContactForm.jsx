038699dc86f4aeb3875290e8f4d88438b8425848
Matthew Spencer <spencer.ma@protonmail.com>
2024-02-26 14:16:35 +0000

    Add netlify attribute to form to test form collection

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { Roboto_Flex } from "next/font/google";
 
 const roboto = Roboto_Flex({
@@ -7,13 +6,15 @@ const roboto = Roboto_Flex({
 
 export const ContactForm = () => {
   return (
-    <form className="flex flex-col">
+    <form className="flex flex-col" name="contact" method="POST" netlify>
       <input
+        name="email"
         className={`${roboto.className} mb-5 w-full rounded-xl p-2 sm:max-w-md`}
         type="text"
         placeholder="Your email address"
       />
       <textarea
+        name="message"
         className={`${roboto.className} mb-5 w-full rounded-xl p-2 sm:max-w-md`}
         placeholder="Your message"
       ></textarea>
@@ -21,10 +22,6 @@ export const ContactForm = () => {
         className={`${roboto.className} mb-5 w-full cursor-pointer rounded-xl bg-orange-500 p-2 text-white sm:max-w-56`}
         type="submit"
         value="Send"
-        onClick={(e) => {
-          e.preventDefault();
-          console.log("clicked");
-        }}
       />
     </form>
   );
